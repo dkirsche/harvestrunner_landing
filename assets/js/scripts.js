6e1c563b16ede4e4cb6bebbb96a5117863800058
Daniel Kirsche <dan.kirsche@gmail.com>
2016-05-23 22:46:18 +0000

    a bunch of commits including refactor to become home page for the ecommerce site

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -51,7 +51,7 @@ jQuery(document).ready(function() {
 	    Countdown initializer
 	*/
 	var now = new Date();
-	var countTo = '2016/01/09';
+	var countTo = '2016/03/19';
 	$('.timer').countdown(countTo, function(event) {
 		$(this).find('.days').text(event.offset.totalDays);
 		$(this).find('.hours').text(event.offset.hours);
